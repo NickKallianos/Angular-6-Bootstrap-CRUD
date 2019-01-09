dda037c3403374b156e020b2258cc44bbc910cc4
nikoskallianos <nikoskallianos@gmail.com>
2019-01-09 18:13:44 +0000

    commit

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,12 @@
+// class modelling
+export class Employee {
+id: number;
+name: string;
+email: string;
+phoneNumber: number;
+contactPreference: string;
+dateOfBirth: Date;
+department: string;
+isActive: boolean;
+photoPath: string;
+}
