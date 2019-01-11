9dcf9e9ff1404c690c40569bab488e5616a6cd52
nikoskallianos <nikoskallianos@gmail.com>
2019-01-11 19:28:31 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+// directives
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
